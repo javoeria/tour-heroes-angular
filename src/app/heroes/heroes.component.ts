b2c8c0159b014c7ff723ccadf9859d257e671a7a
Javier <javier.rb_96@hotmail.com>
2025-04-23 16:24:04 +0200

    create heroes component

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
